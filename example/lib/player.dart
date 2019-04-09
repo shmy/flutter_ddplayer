ea2c66aa50f8ea46ea1c8b359d27d7223d09d7c2
shmy <914111374@qq.com>
2019-04-09 18:51:05 +0800

    优化控制条

--- example/lib/player.dart
+++ example/lib/player.dart
@@ -1,14 +1,44 @@
 import 'package:dd_player/player.dart';
 import 'package:dd_player/screen.dart';
 import 'package:flutter/material.dart';
-class  Player extends StatelessWidget {
+class Player extends StatefulWidget {
+  @override
+  PlayerState createState() => new PlayerState();
+}
+
+class PlayerState extends State<Player> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
 //        appBar: AppBar(
 //          title: const Text('Plugin example app'),
 //        ),
-        body: DdPlayer(url: "https://hair.jingpin88.com/20171029/Fillu3JT/index.m3u8", enableDLNA: true, enablePip: true,),
+      body: DdPlayer(url: "https://hair.jingpin88.com/20171029/Fillu3JT/index.m3u8", enableDLNA: true, enablePip: true,),
     );
+  }
+  @override
+  void initState() {
+    // TODO: implement initState
+    setNormallyOn();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    unSetNormallyOn();
+    super.dispose();
+  }
+
+  @override
+  void didUpdateWidget(Player oldWidget) {
+    // TODO: implement didUpdateWidget
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  void didChangeDependencies() {
+    // TODO: implement didChangeDependencies
+    super.didChangeDependencies();
   }
 }
